4bd5e536061412bb66c0f5c6fa390e8a56c93aad
Augusto <augustooliveioxa@gmail.com>
2022-03-22 20:49:17 -0300

    Tirando propiedade de navegação de Interresados

--- Models/Interessados.cs
+++ Models/Interessados.cs
@@ -4,9 +4,7 @@ namespace AmoPatass
 {
     public class Interessados
     {
-        public Pessoas Pessoas { get; set; }
         public int IdPessoa { get; set; }
-        public Pets Pets { get; set; }
         public int idAnimal { get; set; }
         public DateTime dtInteresse { get; set; }
     }
