9f016ec9b0ea2673423f0e684e1e4a1acbd3bea8
Augusto <augustooliveixa@gmail.com>
2022-03-22 14:50:18 -0300

    Comentando codigo da SexoController

--- Controllers/SexosController.cs
+++ Controllers/SexosController.cs
@@ -8,7 +8,7 @@ namespace TCC_AmoPatas.Controllers
 {
     [ApiController]
     [Route("[Controller]")]
-    public class SexosController : ControllerBase
+    public class SexosController : ControllerBase // heranca da classe ControllerBase, herdando caracteristicas (metodos e etc)
     {
         private readonly DataContext _context; //variavel _context = sempre usaremos para acessa o banco
         private readonly IHttpContextAccessor _httpContextoAccessor;
@@ -16,25 +16,25 @@ namespace TCC_AmoPatas.Controllers
         // Criando Construtor , para inicializar o contexto Declarado !
         public SexosController(DataContext context, IHttpContextAccessor httpContextAccessor)
         {
-            _context = context; //inicialização do atributo
+            _context = context; 
             _httpContextoAccessor = httpContextAccessor;
         }
 
-        [HttpGet]
+        [HttpGet] // metodo HTTP de busca
         public async Task<IActionResult> GetAllAsync()
         {
             try
             {
-                List<Sexo> sex = await _context.Sexo.ToListAsync();
-                if(sex.Count == 0)
+                List<Sexo> sex = await _context.Sexo.ToListAsync(); // acessa o contexto do banco de dados e adiciona em uma lista
+                if(sex.Count == 0) // se não tiver nehuma informação na busca é declarado uma mensagem
                 {
-                    throw new System.Exception("Não foi encotrado nenhum 'Sexo'");
+                    throw new System.Exception("Não foi encotrado nenhum 'Sexo'"); // mensagem de badrequest (status:400)
                 }
-                return Ok(sex);
+                return Ok(sex); //retorna valor de busca
             }
             catch (System.Exception ex)
             {
-                return BadRequest(ex.Message);
+                return BadRequest(ex.Message); // exbibe mensagem de  declarada no try(syste.Exception)
             }
         }
     }
