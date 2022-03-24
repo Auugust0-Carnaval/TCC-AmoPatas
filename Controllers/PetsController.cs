99c88c40d8b3658bc76dc6e9fda7a0e7bff9c1db
Siangui <sisiestudos@gmail.com>
2022-03-24 16:34:18 -0300

    Adicionando e arrumando metodos HTTPS

--- Controllers/PetsController.cs
+++ Controllers/PetsController.cs
@@ -3,6 +3,10 @@ using AmoPatass;
 using AmoPatass.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Data.SqlClient;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace TCC_AmoPatas
 {
@@ -68,14 +72,37 @@ namespace TCC_AmoPatas
                 await _context.Pets.AddAsync(novoAnimal);
                 await _context.SaveChangesAsync();
 
-                return Ok(String.Format("Animal: {0} adicionado com sucesso", novoAnimal.IdAnimal));
-
+                return Ok(String.Format("Animal: {0} adicionado com sucesso", novoAnimal.nmAnimal));
             }
             catch (Exception ex)
             {
                 return BadRequest(ex.Message);
             }
         }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(Pets newPet)
+        {
+            try
+            {
+                String connectionData = "Data Source= workstation id=DB-DS-AMOPATAS.mssql.somee.com;packet size=4096;user id=saaugustocarnaval;pwd=123456789;data source=DB-DS-AMOPATAS.mssql.somee.com;persist security info=False;initial catalog=DB-DS-AMOPATAS";
+                SqlConnection connection = new SqlConnection(connectionData);
+                String querySql = string.Format("SELECT nmAnimal FROM Pets WHERE IdAnimal = {0}", newPet.IdAnimal,connection);
+                connection.Open();
+                SqlCommand cmd = new SqlCommand(querySql,connection);
+                string oldPet = Convert.ToString(cmd.ExecuteScalar());
+
+                _context.Pets.Update(newPet);
+                int linhasAfetadas = await _context.SaveChangesAsync();
+
+                return Ok(String.Format("{0} teve dados alterados",oldPet));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
 
 
 
