85e7ca2fb34d2d2a9168200395755325a13b26d2
Nilton Sales <80773214+Jnrrr@users.noreply.github.com>
2022-03-29 03:31:06 -0300

    Add ForeignKey
    adicionando ForeignKey classe raca

--- Models/Raca.cs
+++ Models/Raca.cs
@@ -1,12 +1,16 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AmoPatass
 {
     public class Raca
     {
         public int IdRaca { get; set; }
-        public Categoria Categoria { get; set; }
+
         public int IdCategoria { get; set; }
+
+        [ForeignKey("IdCategoria")]
+        public Categoria Categoria { get; set; }
         public string dsRaca { get; set; }
     }
 }
