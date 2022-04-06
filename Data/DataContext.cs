e897964bc4185bfe8b20888616b3b09fe001ee22
Sianguii <sisiestudos@gmail.com>
2022-04-06 16:35:59 -0300

    Mudando nomes da Dbset e values das tabelas

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -6,14 +6,9 @@ namespace AmoPatass.Data
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
-
-
         }
 
-
-
-
-         public DbSet<AnimalFoto> AnimalFoto { get; set; }//Pega as classes em models e transforma em tabelas
+         public DbSet<AnimalFoto> AnimaisFoto { get; set; }//Pega as classes em models e transforma em tabelas
          public DbSet<PessoaFoto> PessoaFoto {get; set;}//Classe PessoaFoto vira tabela PessoasFotos
          public DbSet<Categoria> Categoria { get; set; }
          public DbSet<Interessado> Interessados { get; set; }
@@ -23,7 +18,7 @@ namespace AmoPatass.Data
          public DbSet<Preferencia> Preferencia { get; set; }
          public DbSet<Raca> Raca { get; set; }
          public DbSet<Sexo> Sexo { get; set; }
-         public DbSet<Situacao> Situacoe { get; set; }
+         public DbSet<Situacao> Situacao { get; set; }
 
          protected override void OnModelCreating(ModelBuilder modelBuilder)
          {
@@ -42,21 +37,14 @@ namespace AmoPatass.Data
             modelBuilder.Entity<Interessado>()
                 .HasKey(i => new{i.IdPessoa, i.idAnimal});
 
-
             modelBuilder.Entity<Pessoa>()
-
-
                 .HasKey(p => new{p.IdPessoa});
-
-
-
 
             modelBuilder.Entity<Pet>()
                 .HasKey(pt => new{pt.IdAnimal});
 
             modelBuilder.Entity<Raca>()
                 .HasKey(r => new{r.IdRaca});
-
 
             modelBuilder.Entity<Sexo>()
                 .HasKey(s => new{s.IdSexo});
