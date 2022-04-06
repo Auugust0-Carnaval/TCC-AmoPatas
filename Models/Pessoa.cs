40211dabadded824a66a7b668d0f4ea337951942
Augusto <augustooliveixa@gmail.com>
2022-04-05 22:29:14 -0300

    Adicionando propriedades de criptografia

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -13,5 +13,13 @@ namespace AmoPatass
         public string Descricao { get; set; }
         public byte[]? FotoPerfil { get; set; }
         public string? RedeSocial { get; set; }
+
+        //senha de acesso
+        public byte[] PasswordHash { get; set; } // comeco da criptografia
+        public byte[] PasswordSalt { get; set; } // final da criptografia
+
+        //password
+
+        public string Password {get; set;} // senha de acesso
     }
 }
