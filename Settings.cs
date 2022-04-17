ced4d59fd4f832604b33a6a8d6d3b532fe699505
Sianguii <sisiestudos@gmail.com>
2022-04-17 14:56:00 -0300

    modelo de SHA256 para criacao do token

--- Settings.cs
+++ Settings.cs
@@ -2,7 +2,7 @@ namespace TCC_AmoPatas
 {
     public static class Settings
     {
-        public static string Secret = "7751a23fa55170a57e90374df13a3ab78efe0e99";
+        public static string Secret = "fedaf7d8863b448e197b9287d492b70e";
         
     }
 }
