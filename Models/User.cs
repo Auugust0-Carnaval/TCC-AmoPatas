9f0a1157dd090a9678d10e55e9e99457a84b003d
Sianguii <sisiestudos@gmail.com>
2022-04-17 14:41:55 -0300

    criando modelo de usuario de acesso a nossa aplicação

---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+namespace TCC_AmoPatas.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string username  { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
